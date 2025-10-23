af01bc3dce805aba03da60e00fe3c948228c1e5e
GuillinJorge <jorgeguillin@hotmail.com>
2025-10-22 22:58:31 -0300

    GET del producto por ID

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ let [, , method, resource] = process.argv;
 method = method.toUpperCase()
 resource = resource.toLowerCase()
 
+if (method == "GET" && resource.startsWith("products/")){
+   let id = resource.split("/")[1];
+   id = parseInt(id);
+
+    if (isNaN (id) || id <= 0){
+         console.log("No es un número");
+    }
+    fetch("https://fakestoreapi.com/products/" + id)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+
+// // const match = argv.find((arg) => /^products\/\d+$/.test(arg));
+// // const id = match ? match.split("/")[1]:null;
+
+// // if (method == "GET" && id){}
+ 
 if (method == "GET" && resource == "products"){
   //  fetch('https://fakestoreapi.com/products')
   //  .then(response => response.json())
